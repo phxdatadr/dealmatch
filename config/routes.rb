b796ec63dad3ec6ab56a37865765fbdffb824898
phxdatadr <bizovation@gmail.com>
2015-03-01 06:59:54 -0700

    setting up welcome views and controllers and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  devise_for :users
+  devise_for :admins
+  devise_for :members
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
